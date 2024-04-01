fedcfd0b186759382aec83cd5e9cf3e5249a67be
yunju <psw05067@naver.com>
2024-04-01 15:25:34 +0900

    js 변경

--- js/index.js
+++ js/index.js
@@ -1,97 +1,117 @@
 //---------------- navBar slideup & down (S) ------------------
-// $(window).scroll(function() {
-// 	let scroll = $(window).scrollTop();
-// 	if (scroll >= 1100) {
-// 		$("header").addClass("nav_up");
-// 	} else {
-// 		$("header").removeClass("nav_up");
-// 	}
-// });
-
 let didScroll;
 let lastScrollTop = 0;
-let delta = 1100;
-let navbarHeight = $('header').outerHeight();
+const delta = 1100;
+const navBar = document.querySelector("header");
+const navbarHeight = navBar.offsetHeight;
 
-$(window).scroll(function(event){
-    didScroll = true;
+window.addEventListener("scroll", function () {
+  didScroll = true;
 });
 
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
+setInterval(function () {
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
 }, 100);
 
 function hasScrolled() {
-    let st = $(this).scrollTop();
-    
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-  
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('header').addClass('nav_up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('header').removeClass('nav_up');
-        }
-    }
+  const st = window.scrollY || document.documentElement.scrollTop;
 
-    lastScrollTop = st;
+  if (Math.abs(lastScrollTop - st) <= delta) return;
+
+  if (st > lastScrollTop && st > navbarHeight) {
+    // Scroll Down
+    navBar.classList.add("nav_up");
+  } else {
+    // Scroll Up
+    if (st + window.innerHeight < document.documentElement.scrollHeight) {
+      navBar.classList.remove("nav_up");
+    }
+  }
+  lastScrollTop = st;
 }
 
-//---------------- navBar slideup & down (E) ------------------
-//-----------------footer dropdown menu (S) ------------------
+// //---------------- navBar slideup & down (E) ------------------
+// //-----------------footer dropdown menu (S) ------------------
 
-$(".footer_dropdown_menu button").click(function(){
-  if($(".footer_dropdown_menu .depth2").hasClass("active")){
-    $(".footer_dropdown_menu .depth2, .footer_arrow").removeClass("active")
-  }else{
-    $(".footer_dropdown_menu .depth2, .footer_arrow").addClass("active")
+const ftDropdownBtn = document.querySelector(".footer_dropdown_menu button");
+const ftDropdown = document.querySelector(".footer_dropdown_menu .depth2");
+const ftArrow = document.querySelector(".footer_arrow");
+
+ftDropdownBtn.addEventListener("click", function () {
+  if (ftDropdown.classList.contains("active")) {
+    ftDropdown.classList.remove("active");
+    ftArrow.classList.remove("active");
+  } else {
+    ftDropdown.classList.add("active");
+    ftArrow.classList.add("active");
   }
-})
+});
 
-$(".footer_dropdown_menu button").blur(function(){
-  $(".footer_dropdown_menu .depth2, .footer_arrow").removeClass("active")
-})
+ftDropdownBtn.addEventListener("blur", function () {
+  ftDropdown.classList.remove("active");
+  ftArrow.classList.remove("active");
+});
+// //---------------- footer dropdown menu (E) ------------------
+// //---------------- 반응형 드롭다운 메뉴 (S) ------------------
+const body = document.querySelector("body");
+const hambergerBtn = document.querySelector(".hamberger_btn");
+const nav = document.querySelector("nav");
+const navRightBtn = document.querySelector(".right_btn");
+const line1 = document.querySelector(".line1");
+const line2 = document.querySelector(".line2");
+const line3 = document.querySelector(".line3");
 
-//---------------- footer dropdown menu (E) ------------------
-//---------------- 반응형 드롭다운 메뉴 (S) ------------------
-
-$(".hamberger_btn").click(function(){
-  if($("header").hasClass("active")){
-    $("header, nav ,.right_btn").removeClass("active")
-    $(".line1").removeClass("active1")
-    $(".line2").removeClass("active2")
-    $(".line3").removeClass("active3")
-    $("header").off('scroll touchmove mousewheel'); 
-  }else{
-    $("header, nav ,.right_btn").addClass("active")
-    $(".line1").addClass("active1")
-    $(".line2").addClass("active2")
-    $(".line3").addClass("active3")
-    $("header").on('scroll touchmove mousewheel', function(e){
-      e.preventDefault();
-      e.stopPropagation(); 
-      return false;
-      })
+hambergerBtn.addEventListener("click", function () {
+  if (navBar.classList.contains("active")) {
+    navBar.classList.remove("active");
+    nav.classList.remove("active");
+    navRightBtn.classList.remove("active");
+    line1.classList.remove("active1");
+    line2.classList.remove("active2");
+    line3.classList.remove("active3");
+    body.classList.remove("scrollLock");
+  } else {
+    navBar.classList.add("active");
+    nav.classList.add("active");
+    navRightBtn.classList.add("active");
+    line1.classList.add("active1");
+    line2.classList.add("active2");
+    line3.classList.add("active3");
+    body.classList.add("scrollLock");
   }
-})
+});
 
-$("header .depth1 > li > a").click(function(){
-  let dropDownIndex = $(this).parent().index()
-  if($(document).width() < 1025) {
-    if($("header .depth1 > li").eq(dropDownIndex).children(".depth2").hasClass("active") ){
-      $("header .depth1 > li").eq(dropDownIndex).children(".depth2").removeClass("active");
-      $("header .depth1 > li").eq(dropDownIndex).children("a").addClass("arrow").removeClass("change");
-    }else{
-      $("header .depth1 > li").eq(dropDownIndex).children(".depth2").addClass("active");
-      $("header .depth1 > li").eq(dropDownIndex).children("a").removeClass("arrow").addClass("change");
-    }
+function initializeMenu() {
+  let windowWidth = window.innerWidth;
+  let headerMenus = document.querySelectorAll("header .depth1 > li");
+
+  if (windowWidth <= 1025) {
+    headerMenus.forEach((headerMenu) => {
+      let navArr = headerMenu.querySelector(".arrow");
+      if (navArr) {
+        navArr.addEventListener("click", function () {
+          let parentElement = this.parentElement;
+          let depth2 = parentElement.querySelector(".depth2");
+          let isActive = depth2.classList.contains("active");
+          if (isActive) {
+            depth2.classList.remove("active");
+            this.classList.add("arrow");
+            this.classList.remove("change");
+          } else {
+            depth2.classList.add("active");
+            this.classList.remove("arrow");
+            this.classList.add("change");
+          }
+        });
+      }
+    });
   }
-})
+}
 
-//---------------- 반응형 드롭다운 메뉴 (E) ------------------
+window.onload = initializeMenu;
+window.onresize = initializeMenu;
+
+// //---------------- 반응형 드롭다운 메뉴 (E) ------------------
